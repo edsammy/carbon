bf69696bc353a21545cd73f24e8d560cd15aa777
Brad Barbin <bradbarbin@protonmail.com>
2025-09-21 15:04:53 -0400

    ci: add suppport for self-hosted migrations

--- ci/src/migrations.ts
+++ ci/src/migrations.ts
@@ -43,35 +43,31 @@ async function migrate(): Promise<void> {
 
   for await (const customer of customers as Customer[]) {
     try {
-      console.log(`✅ 🥚 Migrating ${customer.name}`);
+      console.log(`✅ 🥚 Migrating ${customer.id}`);
       const {
         database_url,
         decrypted_database_password,
         decrypted_service_role_key,
         project_id,
+        decrypted_access_token,
       } = customer;
-      if (
-        !database_url ||
-        !project_id ||
-        !decrypted_database_password ||
-        !decrypted_service_role_key
-      ) {
-        console.log(`🔴🍳 Missing keys for ${customer.name}`);
+      if (!database_url) {
+        console.log(`🔴🍳 Missing database url for ${customer.id}`);
         continue;
       }
 
-      console.log(`✅ 🔑 Setting up environment for ${customer.name}`);
+      console.log(`✅ 🔑 Setting up environment for ${customer.id}`);
 
       let $$ = $({
         env: {
           SUPABASE_ACCESS_TOKEN:
-            customer.decrypted_access_token === null
+            decrypted_access_token === null
               ? SUPABASE_ACCESS_TOKEN
-              : customer.decrypted_access_token,
+              : decrypted_access_token,
           SUPABASE_URL: database_url,
-          SUPABASE_DB_PASSWORD: decrypted_database_password,
-          SUPABASE_PROJECT_ID: project_id,
-          SUPABASE_SERVICE_ROLE_KEY: decrypted_service_role_key,
+          SUPABASE_DB_PASSWORD: decrypted_database_password ?? undefined,
+          SUPABASE_PROJECT_ID: project_id ?? undefined,
+          SUPABASE_SERVICE_ROLE_KEY: decrypted_service_role_key ?? undefined,
           SUPABASE_AUTH_EXTERNAL_GOOGLE_CLIENT_ID,
           SUPABASE_AUTH_EXTERNAL_GOOGLE_CLIENT_SECRET,
           SUPABASE_AUTH_EXTERNAL_GOOGLE_REDIRECT_URI,
@@ -79,16 +75,21 @@ async function migrate(): Promise<void> {
         cwd: "supabase",
       });
 
-      await $$`supabase link`;
+      let dbArg = "";
+      if (project_id) {
+        await $$`supabase link`;
+      } else if (database_url.startsWith("postgresql://")) {
+        dbArg = `  --db-url ${database_url}`;
+      }
 
-      console.log(`✅ 🐣 Starting migrations for ${customer.name}`);
-      await $$`supabase db push --include-all`;
-      console.log(`✅ 🐣 Starting deployments for ${customer.name}`);
-      await $$`supabase functions deploy`;
+      console.log(`✅ 🐣 Starting migrations for ${customer.id}`);
+      await $$`supabase db push --include-all${dbArg}`;
+      console.log(`✅ 🐣 Starting deployments for ${customer.id}`);
+      await $$`supabase functions deploy${dbArg}`;
 
       if (!customer.seeded) {
         try {
-          console.log(`✅ 🌱 Seeding ${customer.name}`);
+          console.log(`✅ 🌱 Seeding ${customer.id}`);
           await $$`tsx ../../packages/database/src/seed.ts`;
           const { error } = await client
             .from("customer")
@@ -97,19 +98,19 @@ async function migrate(): Promise<void> {
 
           if (error) {
             throw new Error(
-              `🔴 🍳 Failed to mark ${customer.name} as seeded: ${error.message}`
+              `🔴 🍳 Failed to mark ${customer.id} as seeded: ${error.message}`
             );
           }
 
           // TODO: run the seed.sql file
         } catch (e) {
-          console.error(`🔴 🍳 Failed to seed ${customer.name}`, e);
+          console.error(`🔴 🍳 Failed to seed ${customer.id}`, e);
         }
       }
 
-      console.log(`✅ 🐓 Successfully migrated ${customer.name}`);
+      console.log(`✅ 🐓 Successfully migrated ${customer.id}`);
     } catch (error) {
-      console.error(`🔴 🍳 Failed to migrate ${customer.name}`, error);
+      console.error(`🔴 🍳 Failed to migrate ${customer.id}`, error);
     }
   }
 }
